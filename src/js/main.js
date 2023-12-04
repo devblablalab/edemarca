659cfa6488bd939622ef1c002a02a9e5afbc26a1
karinemso <karinemsoliveira@gmail.com>
2023-12-04 09:15:57 -0300

    Splash screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,23 @@
 import { renderShirts } from './shirts.js';
 import { ajaxPromiseShirtsData } from './ajax.js';
 
+
+
+
 $(document).ready(function() {
     async function initShirts() {
       const data = await ajaxPromiseShirtsData();
       renderShirts(data.reverse());
+
+      $('#splash-right').animate({
+        'left': "-500000px",
+    }, 3000);
+    
+    $('#splash-left').animate({
+        'left': "500000px",
+    }, 3000, function() {
+        $('#splash-screen').remove();
+    });
     }
 
     initShirts();
