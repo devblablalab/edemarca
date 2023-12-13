5c74f4c47dbafabda7584558e3a6a93129ba0e8d
Gabriel Albernaz Campos de Freitas <40902456+GabrielAlbernaz-Dev@users.noreply.github.com>
2023-12-13 09:30:36 -0300

    Fix splash screen inc

---
+++ inc/splashScreen.php
@@ -0,0 +1,8 @@
+<div id="splash-screen">
+    <div id="splash-right">
+        <div class="handle"></div>
+    </div>
+    <div id="splash-left">
+        <div class="handle"></div>
+    </div>
+</div>
