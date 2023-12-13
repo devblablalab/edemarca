4050418e5dcb6d007a5a33d40989f56a4dec2256
devblablalab <devblablalab@gmail.com>
2023-12-13 04:29:06 -0300

    First PHP changes

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+
+<?php
+    require_once __DIR__ . '/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
