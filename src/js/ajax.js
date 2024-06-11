d8ca65dfdc644e4442969d1b7ae1dda52e99fdcb
devblablalab <devblablalab@gmail.com>
2024-06-11 03:49:57 -0300

    fix json data

--- src/js/ajax.js
+++ src/js/ajax.js
@@ -1,5 +1,5 @@
 import { getAjaxOptionsJson } from "./utils.js";
 
 export function ajaxPromiseShirtsData() {
-    return $.ajax(getAjaxOptionsJson('https://devblablalab.github.io/edemarca/public/data.json'));
+    return $.ajax(getAjaxOptionsJson(`${window.location.origin}/public/data.json`));
 }
